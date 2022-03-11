5ba7bff77e584143b85ca5d58bc26dab15e8997d
иван <buddazbs@yandex.ru>
2022-03-11 20:35:51 +0300

    Create RunJob.cs
    Create a RunJob Attribute fot Task EventPublishMessage

---
+++ Pickpoint.RMQ.Publisher/Attributs/RunJob.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Pickpoint.RMQ.Publisher.Controllers
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class RunJob : Attribute, IEquatable<RunJob>
+    {
+        public RunJob(string cronSchedule)
+        {
+            this.CronSchedule = cronSchedule ?? "0/1 * * * * ?"; //default every second
+        }
+        public string CronSchedule { get; }
+        public bool Equals(RunJob other)
+        {
+
+            return this.CronSchedule == other.CronSchedule;
+
+        }
+        public override int GetHashCode()
+        {
+            return this.CronSchedule.GetHashCode();
+        }
+    }
+}
